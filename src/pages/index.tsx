aeb62115b56eaca07d826013002801f1dd28879b
Volkan Albayrak <volkanalb@gmail.com>
2023-08-27 00:40:24 +0300

    remove signin signout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,11 +16,47 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <div>
-            {!user.isSignedIn && <SignInButton />}
-            {!!user.isSignedIn && <SignOutButton />}
-            
+        <div className=" ">
+          <div className="flex flex-col">
+            <div className="border-b ">
+              <div className="border-b ">
+                <div className="overflow-hidden">
+                  <table className="min-w-full text-left text-sm font-light">
+                    <thead className="border-b font-medium dark:border-neutral-1500">
+                      <tr>
+                        <th scope="col" className="px-6 py-4">#</th>
+                        <th scope="col" className="px-6 py-4">First</th>
+                        <th scope="col" className="px-6 py-4">Last</th>
+                        <th scope="col" className="px-6 py-4">Handle</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                        <td className="whitespace-nowrap px-6 py-4">Mark</td>
+                        <td className="whitespace-nowrap px-6 py-4">Otto</td>
+                        <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                      </tr>
+                      <tr
+                        className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
+                        <td className="whitespace-nowrap px-6 py-4">Jacob</td>
+                        <td className="whitespace-nowrap px-6 py-4">Thornton</td>
+                        <td className="whitespace-nowrap px-6 py-4">@fat</td>
+                      </tr>
+                      <tr
+                        className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
+                        <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                        <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                        <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
